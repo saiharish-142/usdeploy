142a345770871ecf2075d3dc43209125295e956f
Priority-Pulse-org <prioritypulse@gmail.com>
2021-01-19 22:46:31 +0530

    v2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a44788edfd45a4d981249a6260f06bb1.js"
+  "/precache-manifest.f8ba747db95297ac5ba5ef6aa6ccd403.js"
 );
 
 self.addEventListener('message', (event) => {
